d0798914199525537b80c2d798adcf024fa4ead2
piyushbest <42518831+piyushbest@users.noreply.github.com>
2022-05-11 12:25:44 +0530

    fix  error

--- src/Piyush/Arena/Scheduler.php
+++ src/Piyush/Arena/Scheduler.php
@@ -139,6 +139,7 @@ class Scheduler extends Task
             $humans = $this->plugin->plugin->getServer()->getWorldManager()->getWorldByName($world)->getEntities();
             foreach ($humans as $human)
                 if ($human instanceof NPChuman) {
+                    if(!is_null($human){
                     $ran = $this->plugin->plugin->emptyArenaChooser->getRandomArena();
                         if (!empty($ran->data["world"])) {
                             $human->setNameTag(TextFormat::RED . "TheBridge \n". TextFormat::AQUA . $ran->data["world"] . "\n" . TextFormat::BLUE . count($ran->players) . "/8");
@@ -146,6 +147,7 @@ class Scheduler extends Task
                         } elseif (empty($ran->data["world"])){
                             $human->setNameTag(TextFormat::RED . "THEBRIDGE\n" . TextFormat::AQUA . "ARENAS NOT FOUND");
                         }
+                    }
                 }
         }
 
